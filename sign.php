667645c0c8bd3d9ba1a5983b9a33daf6bc9bddd2
ip31user <f100r6a11ist@gmail.com>
2020-06-03 17:27:44 +0300

    first commit

---
+++ sign.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<body>
+  <form method="POST">
+
+      <input type="text" name="username" placeholder="username" required>
+
+      <input type="password" name="password" placeholder="password" required>
+ 
+      <input type="submit" name="submitButton" value="SUBMIT"> 
+  
+  </form> 
+  <a href="http://localhost:8080/university/forma.php">Do you need an account? Sign up here!</a>
+</body>
+</html>
